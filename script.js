3b3b1922dd448eceed27647fae1ebfb2fcdeb57c
Danielle Sales <dso@cin.ufpe.br>
2017-10-03 19:47:31 -0300

    Point removed when double clicked

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ class Point {
 	constructor(x, y){
 		this.x = x;
 		this.y = y;
+		this.radius = 10
 	}
 
 	draw() {
 		context.beginPath();
 		context.fillStyle = 'grey';
-		context.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+		context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
 		context.fill();
 	}
+
+	clicked(x, y){
+		var vector = new Point(this.x - x, this.y - y);
+		return Math.sqrt(vector.x*vector.x + vector.y*vector.y) <= this.radius;
+	}
+}
+
+function getClicked(point){
+	for(var p in points){
+		if(points[p].clicked(point.x, point.y)){
+			return p;
+		}
+	}
+	return -1;
 }
 
 function drawPoints() {
@@ -32,13 +47,29 @@ function resizeCanvas() {
 }
 
 var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d')
-var points = []
+var context = canvas.getContext('2d');
+var points = [];
+var indexClicked = -1;
 
 resizeCanvas();
 
-canvas.addEventListener('mousedown', e => {
+canvas.addEventListener('mousedown', function(e){
 	var p = new Point(e.offsetX, e.offsetY);
-	points.push(p);
-	draw();
+	indexClicked = getClicked(p);
+	if(indexClicked === -1){
+		points.push(p);
+		draw();
+	}	
+});
+
+canvas.addEventListener('mouseup', function(e){
+	indexClicked = -1;
+});
+
+canvas.addEventListener('dblclick', function(e){
+	var indexClicked = getClicked(new Point(e.offsetX, e.offsetY));
+	if(indexClicked !== -1){
+		points.splice(indexClicked, 1);
+		draw();
+	}
 });
